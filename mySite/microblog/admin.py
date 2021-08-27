ca7fe5eb3e002d6effc5f8dc0fea5c03337282f7
ALittleMoron <dima.lunev14@gmail.com>
2021-08-28 01:19:12 +0300

    админка

--- mySite/microblog/admin.py
+++ mySite/microblog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'created_at', 'updated_at', 'published_at', 'is_published')
+    list_display_links = ('id', 'title')
+    search_fields = ('title', 'tags')
+    list_editable = ('is_published',)
+    list_filter = ('is_published', 'tags')
+
+
+admin.site.register(Post, PostAdmin)
