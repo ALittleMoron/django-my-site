8a9270c25e91623ca9cc1c671e745014f54f014e
ALittleMoron <dima.lunev14@gmail.com>
2021-08-27 23:33:14 +0300

    Заглушка для страницы микроблога, а также отображение текущей страницы в навигационной панели

--- mySite/microblog/urls.py
+++ mySite/microblog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import HomePage
+from .views import HomePage, BlogList
 
 urlpatterns = [
-    path('', HomePage.as_view(), name='homePage')
+    path('', HomePage.as_view(), name='homePage'),
+    path('microblog/', BlogList.as_view(), name="microblog/blogList")
 ]
--- mySite/microblog/views.py
+++ mySite/microblog/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
-from django.views.generic import View
+from django.views.generic import View, ListView
 
 
 class HomePage(View):
@@ -11,5 +11,14 @@ class HomePage(View):
     http_method_names = ['get']
     template_name = 'microblog/homePage.html'
     
+    def get(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
+        return render(request, self.template_name)
+
+
+class BlogList(View):
+    http_method_names = ['get']
+    template_name = 'microblog/blogList.html'
+
+
     def get(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
         return render(request, self.template_name)
