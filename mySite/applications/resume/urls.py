9fef9fe344ee7ee4db8f9582c3dd911ec10ee4d0
ALittleMoron <dima.lunev14@gmail.com>
2021-10-06 00:06:02 +0300

    Базовый шаблон для резюме, отказ от автобиографии, навигация для раздела

--- mySite/applications/resume/urls.py
+++ mySite/applications/resume/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 
-from .views import AboutMe, Resume, GitHubProjects
+from .views import Resume, GitHubProjects
 
 
 urlpatterns = [
     path('', Resume.as_view(), name='resume'),
-    path('about-me/', AboutMe.as_view(), name='resume/aboutMe'),
     path('git-hub-projects/', GitHubProjects.as_view(), name='resume/gitHubProjects'),
 ]
--- mySite/applications/resume/views.py
+++ mySite/applications/resume/views.py
@@ -6,9 +6,5 @@ class Resume(GetView):
     template_name = 'resume/resume.html'
 
 
-class AboutMe(GetView):
-    template_name = 'resume/aboutMe.html'
-
-
 class GitHubProjects(GetView):
     template_name = 'resume/gitHubProjects.html'
