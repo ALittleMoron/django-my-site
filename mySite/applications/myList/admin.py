97f793620747cf9b04d954402782938084b839d5
ALittleMoron <dima.lunev14@gmail.com>
2021-11-04 20:25:31 +0300

    небольшая переработка фильтра рекомендации; dummy-изображение для постеров в админке и в productDetail

--- mySite/applications/myList/admin.py
+++ mySite/applications/myList/admin.py
@@ -2,6 +2,7 @@ from ckeditor.widgets import CKEditorWidget
 from django import forms
 from django.contrib import admin
 from django.utils.html import format_html
+from django.templatetags.static import static
 
 from .models import Product
 
@@ -26,18 +27,22 @@ class CommonAdmin(admin.ModelAdmin):
         "native_name",
         "rating",
         "i_recommend",
+        "is_published",
         "poster_preview_tag",
     )
     list_display_links = ("id", "name", "native_name")
     search_fields = ("name", "native_name")
-    list_filter = ("i_recommend", "rating")
+    list_filter = ("i_recommend", "product_type", "rating")
     prepopulated_fields = {"slug": ("name",)}
 
     def poster_preview_tag(self, obj: Product):
+        image = '<img src="{}" width="45px" height="45px"/>'
         if not obj.poster:
-            return ''
+            return format_html(
+                image.format(static('dummy.png'))
+            )
         return format_html(
-            '<img src="{}" width="45px" height="45px"/>'.format(obj.poster.url)
+            image.format(obj.poster.url)
         )
 
     poster_preview_tag.allow_tags = True
--- mySite/static/other/filterRecommend.js
+++ mySite/static/other/filterRecommend.js
@@ -1,15 +1,36 @@
-document.querySelector('#IRecommendCheck').addEventListener('click', function(){
+document.querySelector('#filter').addEventListener('click', function(e){
     list = document.querySelector('tbody').querySelectorAll('tr')
-    if (this.checked == true){
-        list.forEach(e => {
-            if (e.innerText.slice(-4).indexOf('Нет') != -1) {
-                e.style = 'display: none;'
-            }
-        });
+    if (e.target && e.target.id == 'IRecommendRadio1'){
+        list.forEach(ev => {
+            ev.style.display = ''
+        })
     }
-    else {
-        list.forEach(e => {
-            e.style = ''
+    else if (e.target && e.target.id == 'IRecommendRadio2'){
+        list.forEach(ev => {
+            ev.style.display = ''
+        })
+        list.forEach(ev => {
+            if (ev.innerText.slice(-4).indexOf('Нет') != -1){
+                ev.style.display = 'none'
+            }
+
+            else if (ev.innerText.slice(-4).indexOf('Да') != -1){
+                ev.style.display = ''
+            }
+        })
+    }
+    else if (e.target && e.target.id == 'IRecommendRadio3'){
+        list.forEach(ev => {
+            ev.style.display = ''
+        })
+        list.forEach(ev => {
+            if (ev.innerText.slice(-4).indexOf('Да') != -1){
+                ev.style.display = 'none'
+            }
+
+            else if (ev.innerText.slice(-4).indexOf('Нет') != -1){
+                ev.style.display = ''
+            }
         })
     }
 });
