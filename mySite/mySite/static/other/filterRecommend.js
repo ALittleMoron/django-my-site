61d05eb52043a0fb7982acd5f278520f8c74911b
ALittleMoron <dima.lunev14@gmail.com>
2022-02-19 13:50:08 +0300

    фильтр по статусу

--- mySite/mySite/static/other/filterRecommend.js
+++ mySite/mySite/static/other/filterRecommend.js
@@ -54,7 +54,39 @@ function hide_empty_tables(){
 }
 
 
+function statusChecks(){
+    const checks = document.querySelector('#statusChecks').querySelectorAll('input');
+    const table_divs = document.querySelectorAll('.tblDiv')
+    var count = 0;
+
+    for (const check of checks){
+        var label = check.nextElementSibling.textContent.split(' ').filter(
+            function x(y){return ['', '\n'].indexOf(y) == -1}
+        ).join(' ').replaceAll('\n', '');
+        var needed_div;
+
+        for (let i = 0; i < table_divs.length; i++){
+            var temp_h5 = table_divs[i].querySelector('h5').innerText
+            if (temp_h5 == label){
+                needed_div = table_divs[i];
+                break;
+            }
+        }
+
+        if (check.checked){
+            needed_div.style.display = '';
+        } else {
+            count++
+            needed_div.style.display = 'none';
+        }
+        if (count == 6){show_all()}
+    }
+
+    count = 0;
+}
+
 document.querySelector('#filter').addEventListener('click', function(e){
+    const flex_arr = ['flexCheck1', 'flexCheck2', 'flexCheck3', 'flexCheck4', 'flexCheck5', 'flexCheck6']
     if (e.target){
         const trs = document.querySelectorAll('tr')
         
@@ -69,5 +101,8 @@ document.querySelector('#filter').addEventListener('click', function(e){
             hide_recommend(trs)
             hide_empty_tables()
         }
+        else if(flex_arr.indexOf(e.target.id) != -1){
+            statusChecks()
+        }
     }
-});
+})
